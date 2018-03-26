498675cc9f07e7e41c23191063c861a4adb53b99
Munteanu Andrei-Stefan <munteanu.stefan@outlook.com>
2018-03-26 08:32:54 +0300

    Create Brain class
    This closes #1

---
+++ src/Brain.java
@@ -0,0 +1,13 @@
+public class Brain {
+
+    public Brain(){
+        System.out.println("Brain was created");
+    }
+
+    public static void Analyze (File file){
+        Recognition rec = null;
+        //reflection  here
+    }
+
+
+}
